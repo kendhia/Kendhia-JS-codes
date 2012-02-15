e84f898c4cb0fc9ed502ded0113f088985158d0c
Kendhia <kendhia@gmail.com>
2012-02-15 20:59:40 +0100

    Added a test for the admin page !!

--- OnePic/OnePic.php
+++ OnePic/OnePic.php
@@ -6,19 +6,16 @@ Author: Kendhia
 Description: The plugin detects all pictures on the document(page) that have an ID and turn them all to one picture chosen by the user .
 Version: Beta
 Author URI : kendhiaa.tk
-Email: kendhia@gmail.com
 */
 echo '<script type="text/javascript">'
-   , 'window.onload = FindPic;
-window.onload = initAll;
+   , 'window.onload = initAll;
 
-
-
-function FindPic() {
+function initAll() {
     for (var i=0; i<document.images.length; i++) {
-        Newimage(document.images[i]);
+        Newimage(document.images[i])
     }
 }
+
 
 function Newimage(thisimg) {
     thisimg.outimage = new Image();
@@ -38,6 +35,26 @@ function Changeout() {
     this.src = this.outimage.src;
 }'
    , '</script>';
-   
 
+?>
+
+<?php
+add_action('admin_menu', 'OnePic_menu');
+
+function OnePic_menu() {
+   add_options_page('OnePic options', 'OnePic', 'manage_options', 'my-unique-identifier', 'OnePic_options');
+}
+
+function OnePic_options() {
+	if (!current_user_can('manage_options'))  {
+		wp_die( __('You do not have sufficient permissions to access this page.') );
+	}
+	echo '<div class="wrap">';
+	echo '<p><a>URL of the picture: </a> <input id="URL"></p>
+    <p><a>In wich part do you want to use this plugin? </a> All pic in the page<input type="checkbox" onClick="justOne1()" id="alls" >
+    Header <input type="checkbox" id="Header" onClick="justOne1()"> Body <input type="checkbox" id="body" onClick="justOne1()"> </a> </p>
+    <p>Event: dbl click<input type="checkbox" id="dblclick" onClick="justOne2()"> click<input type="checkbox" id="click" onClick="justOne2()"> on mouse over<input type="checkbox" id="onmouseover" onClick="justOne2()"></p>
+    <button id="clickbtm" onClick="Checkall()"> Save</button>';
+
+}
 ?>
